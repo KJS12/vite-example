d455babe0069c1392d83770fc90c929aabe229a6
junsu <junsu.kim@danbplus.com>
2025-07-22 17:13:46 +0900

    20250722 KJS - CSS 라이브러리 적용 (tailwind)

--- vite.config.js
+++ vite.config.js
@@ -11,14 +11,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
 import { fileURLToPath } from "url";
+import tailwindcss from '@tailwindcss/vite';
 
 // __dirname 대체 코드
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
+// https://tailwindcss.com
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // @ → src/
